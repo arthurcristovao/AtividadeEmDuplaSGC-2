569ffe5a6c23266f571060ee9fc76e54bd917dd5
Arthur Cristovao <arthhur122@gmail.com>
2023-05-04 20:42:29 -0300

    Arthur att2

--- Calculadora.c
+++ Calculadora.c
@@ -2,16 +2,29 @@
 #include <stdlib.h>
 
 float soma(float n1, float n2);
+float subtracao(float n1, float n2);
+float divisao(float n1, float n2);
+
 
 int main(){
 
-    float num1 = 50, num2 = 50;
+    float num1 = 50, num2 = 2;
 
-    printf("SOMA: %.1f + %.1f = %.1f\n",num1,num2,soma(num1,num2));
-
+    printf("SOMA: %.1f + %.1f = %.1f\n", num1, num2, soma(num1,num2));
+    printf("SUBTRACAO: %.1f - %.1f = %.1f\n", num1, num2, subtracao(num1,num2));
+    printf("DIVISAO: %.1f / %.1f = %.1f\n", num1, num2, divisao(num1,num2));
+    
     return 0;
 }
 
 float soma(float n1, float n2){
         return n1 + n2;
+}
+
+float subtracao(float n1, float n2){
+        return n1 - n2;
+}
+
+float divisao(float n1, float n2){
+        return n1 / n2;
 }
